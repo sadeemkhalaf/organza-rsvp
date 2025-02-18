b5334963957cb33aff6030ee044c2e79cebbfb5b
Sadeem Ahmad <sahmad@first.tech>
2025-02-18 11:31:36 +0300

    initial new-event component

---
+++ src/app/new-event/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewEventPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Create a New Event</h1>
+            {/* Add your form or content here */}
+        </div>
+    );
+};
+
+export default NewEventPage; 
